269966a070fe6aca508c38229629922b7749fd62
syth <cjdeni205@gmail.com>
2022-11-03 13:36:16 -0400

    Various Fixes

--- app.js
+++ app.js
@@ -2,45 +2,50 @@ const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.navbar__menu');
 const navLogo = document.querySelector('#navbar__logo');
 
-//Display mobile menu
-const mobileMenu = () => {
+menu.addEventListener('click', function() {
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
-};
+})
+
+//Display mobile menu
+// const mobileMenu = () => {
+//     menu.classList.toggle('is-active');
+//     menuLinks.classList.toggle('active');
+// };
 
 menu.addEventListener('click', mobileMenu);
 
 // Show active menu when scrolling
-// const highlightMenu = () => {
-//     const elem = document.querySelector('.highlight');
-//     const homeMenu = document.querySelector('#home-page');
-//     const aboutMenu = document.querySelector('#about-page');
-//     const servicesMenu = document.querySelector('#services-page');
-//     let scrollPos = window.scrollY;
+const highlightMenu = () => {
+    const elem = document.querySelector('.highlight');
+    const homeMenu = document.querySelector('#home-page');
+    const aboutMenu = document.querySelector('#about-page');
+    const servicesMenu = document.querySelector('#services-page');
+    let scrollPos = window.scrollY;
 
-//     // adds 'highlight' class to my menu items
-//     if (window.innerWidth > 960 && scrollPos < 600) {
-//     homeMenu.classList.add('highlight');
-//     aboutMenu.classList.remove('highlight');
-//     return;
-//     } else if (window.innerWidth > 960 && scrollPos < 1400) {
-//     aboutMenu.classList.add('highlight');
-//     homeMenu.classList.remove('highlight');
-//     servicesMenu.classList.remove('highlight');
-//     return;
-//     } else if (window.innerWidth > 960 && scrollPos < 2345) {
-//     servicesMenu.classList.add('highlight');
-//     aboutMenu.classList.remove('highlight');
-//     return;
-//     }
+    // adds 'highlight' class to my menu items
+    if (window.innerWidth > 960 && scrollPos < 600) {
+    homeMenu.classList.add('highlight');
+    aboutMenu.classList.remove('highlight');
+    return;
+    } else if (window.innerWidth > 960 && scrollPos < 1400) {
+    aboutMenu.classList.add('highlight');
+    homeMenu.classList.remove('highlight');
+    servicesMenu.classList.remove('highlight');
+    return;
+    } else if (window.innerWidth > 960 && scrollPos < 2345) {
+    servicesMenu.classList.add('highlight');
+    aboutMenu.classList.remove('highlight');
+    return;
+    }
 
-//     if ((elem && window.innerWIdth < 960 && scrollPos < 600) || elem) {
-//     elem.classList.remove('highlight');
-//     }
-// };
+    if ((elem && window.innerWIdth < 960 && scrollPos < 600) || elem) {
+    elem.classList.remove('highlight');
+    }
+};
 
-// window.addEventListener('scroll', highlightMenu);
-// window.addEventListener('click', highlightMenu);
+window.addEventListener('scroll', highlightMenu);
+window.addEventListener('click', highlightMenu);
 
 // Close mobile Menu when clicking on menu item
 const hideMobileMenu = () => {
